b292b7f468fd3eafd1a0fb767742084d6b8764b8
karthik449213 <karthikpiinasi@gmail.com>
2025-05-12 12:51:50 +0530

    Update data.ts

--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -7,7 +7,7 @@ export const programs = [
     name: "HIIT Revolution",
     description: "High-intensity interval training to maximize calorie burn and boost metabolism in just 45 minutes.",
     duration: 45,
-    image: "https://images.unsplash.com/photo-1581009146145-b5ef050c2e1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80"
+    image: "./public/image19.webp"
   },
   {
     id: 2,
